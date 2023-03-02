1f5664b75dcf1075ba888f8259801adb09ac7737
Grace Gicheru <gracegicheru3@gmail.com>
2023-03-02 20:55:57 +0300

    add moringa week6

---
+++ _config.js
@@ -0,0 +1,9 @@
+var config = {}
+
+// Update to have your correct username and password
+config.mongoURI = {
+    production: 'mongodb+srv://gracegicheru3:qUqT9aIu15SkZGIG@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://gracegicheru3:qUqT9aIu15SkZGIG@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://gracegicheru3:qUqT9aIu15SkZGIG@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+}
+module.exports = config;
